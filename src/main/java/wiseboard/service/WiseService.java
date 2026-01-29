083b9e8c36eb513555911ac7e0452095e9028bea
5s-mini <dnclsehd123@naver.com>
2026-01-29 12:31:30 +0900

    feat: CRUD 기능을 구현한  WiseService 클래스 작성

--- src/main/java/wiseboard/service/WiseService.java
+++ src/main/java/wiseboard/service/WiseService.java
@@ -1,4 +1,34 @@
 package wiseboard.service;
 
+import wiseboard.domain.WiseQuote;
+import wiseboard.repository.WiseRepository;
+
 public class WiseService {
+
+    private final WiseRepository wiseRepository;
+
+    public WiseService(WiseRepository wiseRepository) {
+        this.wiseRepository = wiseRepository;
+    }
+
+    public Integer Register(String author, String content) {
+        WiseQuote quote = wiseRepository.Save(author, content);
+        return quote.id();
+    }
+
+    public WiseQuote[] FindAllDesc() {
+        return wiseRepository.FindAllDesc();
+    }
+
+    public boolean Delete(Integer id) {
+        return wiseRepository.DeleteById(id);
+    }
+
+    public WiseQuote FindById(Integer id) {
+        return wiseRepository.FindById(id);
+    }
+
+    public boolean Modify(Integer id, String author, String content) {
+        return wiseRepository.ReplaceById(id, author, content);
+    }
 }
